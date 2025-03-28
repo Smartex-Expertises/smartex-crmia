4199eb1239e2fe6e7e9639fab89b495fb68c4c2d
othinieljo <othinieljo@gmail.com>
2025-03-28 00:27:29 +0000

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  // Ignore les erreurs TypeScript lors du build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  // Ignore les erreurs ESLint lors du build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
